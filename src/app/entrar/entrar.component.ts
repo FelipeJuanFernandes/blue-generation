dc2cd35dffa8386ff1678a6a2e751536582d07bb
Gisele R. Mascarenhas <gisele.mascarenhass@outlook.com>
2021-10-17 18:02:01 -0300

    Ajustes

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -7,38 +7,39 @@ import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
-  styleUrls: ['./entrar.component.css']
+  styleUrls: ['./entrar.component.css'],
 })
+
 export class EntrarComponent implements OnInit {
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
 
-  usuarioLogin: UsuarioLogin = new UsuarioLogin()
-
-  constructor(
-    private auth: AuthService,
-    private router: Router
-  ) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
-    window.scroll(0, 0)
+    window.scroll(0, 0);
   }
 
   entrar() {
-    this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
-      this.usuarioLogin = resp
+    this.auth.entrar(this.usuarioLogin).subscribe(
+      (resp: UsuarioLogin) => {
+        this.usuarioLogin = resp;
 
-      environment.token = this.usuarioLogin.token
-      environment.id = this.usuarioLogin.id
-      environment.nome = this.usuarioLogin.nome
-      environment.tipo = this.usuarioLogin.tipo
-      console.log("🚀 ~ file: entrar.component.ts ~ line 33 ~ EntrarComponent ~ this.auth.entrar ~ environment", environment)
+        environment.token = this.usuarioLogin.token;
+        environment.id = this.usuarioLogin.id;
+        environment.nome = this.usuarioLogin.nome;
+        environment.tipo = this.usuarioLogin.tipo;
+        console.log(
+          '🚀 ~ file: entrar.component.ts ~ line 33 ~ EntrarComponent ~ this.auth.entrar ~ environment',
+          environment
+        );
 
-      this.router.navigate(['/inicio'])
-
-    }, erro => {
-      if (erro.status == 401) {
-        alert('Digite usuário e senha válidos!');
+        this.router.navigate(['/inicio']);
+      },
+      (erro) => {
+        if (erro.status == 401) {
+          alert('Digite usuário e senha válidos!');
+        }
       }
-    })
+    );
   }
-
 }
